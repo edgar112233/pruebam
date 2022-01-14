6e69dbc986cf92fba8a48e148af1b3bf2b012eb8
edgar112233 <correoedgar12@gmail.com>
2022-01-14 11:39:55 -0300

    modulo con promise

--- App.js
+++ App.js
@@ -13,6 +13,33 @@ export default function App() {
     } catch(e) { 
       console.error(e); 
     }
+    try{
+      ToastModule.pcallback(
+        'juan53',
+        'testLocation',
+        (msg) => {
+          console.log(msg);
+        },
+        (x,y,hola,Name)=>{
+          console.log('equis:'+x +'y:'+y+'este es hola:'+hola+'name:'+Name);
+        }
+      );
+    } catch(e){
+      console.error(e);
+    }
+
+    
+    try{
+      var {x, y, APPs, Name} = await ToastModule.ppromise(
+        'juan77',
+        'testLocation');
+
+        console.log('equis con promise:'+x +'y:'+y+'name:'+Name);
+        console.log('las apps instalada:',APPs)
+        
+    } catch(e){
+      console.error(e);
+    }
   }   
     
 
--- android/app/src/main/java/com/pruebam/ToastModule.java
+++ android/app/src/main/java/com/pruebam/ToastModule.java
@@ -9,9 +9,13 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
@@ -24,6 +28,7 @@ public class ToastModule extends ReactContextBaseJavaModule {
 
     private static final String DURATION_SHORT_KEY = "SHORT";
     private static final String DURATION_LONG_KEY = "LONG";
+    private static final String E_LAYOUT_ERROR = "E_LAYOUT_ERROR";
     PackageManager pm;
 
     public ToastModule(ReactApplicationContext reactContext) {
@@ -47,34 +52,97 @@ public class ToastModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void show(String message, int duration) {
 
-        List<ApplicationInfo> packages= pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
         List<ApplicationInfo> installedApps = new ArrayList<ApplicationInfo>();
 
-        for(ApplicationInfo app : packages) {
-                //checks for flags; if flagged, check if updated system app
-                if((app.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) {
-                    installedApps.add(app); //Agregar Apps instaladas
-                    //it's a system app, not interested
-                } else if ((app.flags & ApplicationInfo.FLAG_SYSTEM) != 0) {
-                    //Discard this one
-                    //in this case, it should be a user-installed app
-                    installedApps.add(app);
-                }
+        for (ApplicationInfo app : packages) {
+            // checks for flags; if flagged, check if updated system app
+            if ((app.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) {
+                installedApps.add(app); // Agregar Apps instaladas
+                // it's a system app, not interested
+            } else if ((app.flags & ApplicationInfo.FLAG_SYSTEM) != 0) {
+                // Discard this one
+                // in this case, it should be a user-installed app
+                installedApps.add(app);
+            }
 
-                //else if (app.flags & ApplicationInfo.)
-                else {
-                    installedApps.add(app);
-                }
+            // else if (app.flags & ApplicationInfo.)
+            else {
+                installedApps.add(app);
+            }
         }
 
-        /*List<String> appList=new ArrayList<>();
+        /*
+         * List<String> appList=new ArrayList<>();
+         * 
+         * /*for(ApplicationInfo i : installedApps){
+         * appList.add( (String) i.loadLabel(pm));
+         * }
+         */
 
-        /*for(ApplicationInfo i : installedApps){
-            appList.add( (String) i.loadLabel(pm));
-        }*/
-
-        //System.out.println(installedApps);
+        // System.out.println(installedApps);
         Toast.makeText(getReactApplicationContext(), installedApps.toString(), duration).show();
+
+    }
+
+    @ReactMethod
+    public void pcallback(
+            String name,
+            String location,
+            Callback errorCallback,
+            Callback successCallback) {
+
+        try {
+            int x = 15;
+            int y = 13;
+            String hola = "hola";
+            String Name = "juan";
+            successCallback.invoke(x, y, hola, name);
+        } catch (Error e) {
+            errorCallback.invoke(e.getMessage());
+        }
+
+    }
+
+    @ReactMethod
+    public void ppromise(
+            String name,
+            String location,
+            Promise promise) {
+
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        List<ApplicationInfo> installedApps = new ArrayList<ApplicationInfo>();
+
+        for (ApplicationInfo app : packages) {
+            // checks for flags; if flagged, check if updated system app
+            if ((app.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) {
+                installedApps.add(app); // Agregar Apps instaladas
+                // it's a system app, not interested
+            } else if ((app.flags & ApplicationInfo.FLAG_SYSTEM) != 0) {
+                // Discard this one
+                // in this case, it should be a user-installed app
+                installedApps.add(app);
+            }
+
+            // else if (app.flags & ApplicationInfo.)
+            else {
+                installedApps.add(app);
+            }
+        }
+
+        try {
+
+            WritableMap map = Arguments.createMap();
+
+            map.putDouble("x", 15);
+            map.putInt("y", 13);
+            map.putString("APPs", installedApps.toString());
+            map.putString("Name", name);
+            promise.resolve(map);
+
+        } catch (Error e) {
+            promise.reject(E_LAYOUT_ERROR, e);
+        }
 
     }
 
